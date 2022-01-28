f07255ab5e04665416419b7a77fe6c8753d2b90e
Max <massimo.colucci@citelgroup.it>
2022-01-28 09:49:45 +0100

    Cambio interfaccia per la durata e inserimento controlli

--- AppCinema/App/src/sample/controllers/FilmController.java
+++ AppCinema/App/src/sample/controllers/FilmController.java
@@ -2,6 +2,7 @@ package sample.controllers;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.regex.Pattern;
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
@@ -18,15 +19,18 @@ public class FilmController implements Initializable {
   public TextField Titolo;
   public TextField Trama;
   public TextField Regia;
-  public TextField DurataFilm;
   public ChoiceBox GENERE;
   public Button SalvaButton;
   public Button AnnullaButton;
   public TextField AnnoFilm;
+  public TextField Hour;
+  public TextField minutes;
+  public TextField second;
 
   @Override
   public void initialize(URL url, ResourceBundle resourceBundle) {
     GENERE.getItems().addAll(sample.models.enumerations.GENERE.values());
+    GENERE.setValue(sample.models.enumerations.GENERE.AZIONE);
   }
 
   public void saveFilm(ActionEvent actionEvent) {
@@ -35,7 +39,9 @@ public class FilmController implements Initializable {
     String regia = Regia.getText();
     String genere = GENERE.getValue().toString();
     System.out.println(genere);
-    String durataFilm = DurataFilm.toString();
+    String ore = Hour.getText();
+    String minut = minutes.getText();
+    String secondi = second.getText();
     String annoFilm = AnnoFilm.getText();
 
     if(titolo.isEmpty() || annoFilm.isEmpty() || genere.isEmpty()){
@@ -51,6 +57,20 @@ public class FilmController implements Initializable {
       alert.showAndWait();
     }
 
+    if(ore.length() + minut.length() + secondi.length() != 6){
+      errorFormatLenght();
+    }
+    if( Pattern.matches("[a-zA-Z]+",ore) && Pattern.matches("[a-zA-Z]+",minut) && Pattern.matches("[a-zA-Z]+",secondi)){
+      errorFormatLenght();
+    }
+
+  }
+
+  private void errorFormatLenght() {
+    Alert alert = new Alert(AlertType.ERROR);
+    alert.setHeaderText(null);
+    alert.setContentText("Errore campo durata (HH:MM:SS)");
+    alert.showAndWait();
   }
 
   public void cancelFilm(ActionEvent actionEvent) {
