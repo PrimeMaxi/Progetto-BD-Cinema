94c0c7d63d0095828f5c6a1022b558afcfdaac51
Max <massimo.colucci@citelgroup.it>
2022-02-06 11:53:09 +0100

    Fixed bug trigger generate posto

--- PROGETTAZIONE FISICA/DB.sql
+++ PROGETTAZIONE FISICA/DB.sql
@@ -8,6 +8,8 @@ drop type if exists tecnologia;
 drop type if exists audio;
 drop table if exists cinema;
 
+
+
 CREATE TABLE CINEMA (
 	IdCinema SERIAL PRIMARY KEY,
 	NomeCinema VARCHAR(50) NOT NULL UNIQUE,
@@ -19,13 +21,8 @@ CREATE TABLE CINEMA (
 	CONSTRAINT chk_telefono CHECK (Telefono not like '%[^0-9]%') --Assicura che un numero che inizia con 0 venga inserito nel DB
 );
 
-INSERT INTO CINEMA (NomeCinema,Indirizzo,Provincia,NumeroSala,Città,Telefono)
-VALUES
-('Happy MaxiCinema Afragola','Centro Commerciale Le Porte di Napoli, Via Santa Maria la Nova, 1, 80021 Afragola NA','NA',2,'Afragola','0818607136');
-
 CREATE TYPE TECNOLOGIA AS ENUM ('IMAX','ISense','ScreenX','3D');
 CREATE TYPE AUDIO AS ENUM ('Dolby Digital Surround','Doby Digital Plus');
-
 
 CREATE TABLE SALA (
 	IdSala INTEGER PRIMARY KEY,
@@ -37,11 +34,6 @@ CREATE TABLE SALA (
 	CONSTRAINT fk_Cinema FOREIGN KEY (IdCinemaFk) REFERENCES CINEMA(IdCinema)
 );
 
-INSERT INTO SALA(IdSala,Capienza,IdCinemaFk)
-VALUES
-(1,100,1);
-
-
 CREATE TABLE POSTO(
 	IdPosto SERIAL PRIMARY KEY,
 	FilaX CHAR(1) NOT NULL, --CHECK (UPPER(FilaX) = FilaX) restituisce errore se non si inserisce il carrattere in UPPER
@@ -51,9 +43,7 @@ CREATE TABLE POSTO(
 	CONSTRAINT fk_Sala FOREIGN KEY (IdSalaFk) REFERENCES SALA(IdSala) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-
 CREATE TYPE GENERE AS ENUM ('Azione','Horror','Fantascienza','Comico','Thriller','Western','Documentario','Drammatico');
-
 
 CREATE TABLE FILM (
 	IdFilm SERIAL PRIMARY KEY,
@@ -67,13 +57,6 @@ CREATE TABLE FILM (
 	FineData DATE DEFAULT (CURRENT_DATE + interval '28' DAY), -- se il campo è vuoto, imposta 28 giorni di disponibilità
 	CONSTRAINT chk_durata CHECK(durata > TIME '01:19:00' AND durata < TIME '03:20:00') --Verifica che l'inserimento della durata sia compreso dall'intervallo indicato
 );
-
-INSERT INTO FILM (Titolo,Trama,Regia,Anno,Durata,Genere)
-VALUES 
-('Spider-Man: No Way Home',DEFAULT,'Jon Watts',2021,'02:28:00','Fantascienza'),
-('Matrix Resurrections',DEFAULT,'Lana Wachowski',2022,'02:28:00','Fantascienza'),
-('House of Gucci',DEFAULT,'Ridley Scott',2021,'02:38:00','Drammatico'),
-('No Time To Die',DEFAULT,'Cary Fukunaga',2021,'02:43:00','Azione');
 
 CREATE TABLE PROIEZIONE(
 	IdProiezione SERIAL PRIMARY KEY,
@@ -160,3 +143,27 @@ AFTER insert on SALA
 EXECUTE PROCEDURE generate_Posto();  
 
 ---Funzion e Trigger che elemina un film dopo la scadenza di proiezione FineData
+
+
+
+
+
+
+----------------------------------------------------------------------------------------------------------
+
+---Inserimento dati
+
+INSERT INTO CINEMA (NomeCinema,Indirizzo,Provincia,NumeroSala,Città,Telefono)
+VALUES
+('Happy MaxiCinema Afragola','Centro Commerciale Le Porte di Napoli, Via Santa Maria la Nova, 1, 80021 Afragola NA','NA',2,'Afragola','0818607136');
+
+INSERT INTO SALA(IdSala,Capienza,IdCinemaFk)
+VALUES
+(1,100,1);
+
+INSERT INTO FILM (Titolo,Trama,Regia,Anno,Durata,Genere)
+VALUES 
+('Spider-Man: No Way Home',DEFAULT,'Jon Watts',2021,'02:28:00','Fantascienza'),
+('Matrix Resurrections',DEFAULT,'Lana Wachowski',2022,'02:28:00','Fantascienza'),
+('House of Gucci',DEFAULT,'Ridley Scott',2021,'02:38:00','Drammatico'),
+('No Time To Die',DEFAULT,'Cary Fukunaga',2021,'02:43:00','Azione');
