d789ef6d644f25fc847c80d43dd0b16d00e9e8bc
Max <massimo.colucci@citelgroup.it>
2022-03-13 15:02:10 +0100

    Posto disabilitato alla seleazione se è occupato

--- 4 - AppCinema/App/src/sample/controllers/SeatController.java
+++ 4 - AppCinema/App/src/sample/controllers/SeatController.java
@@ -1,12 +1,7 @@
 package sample.controllers;
 
 import com.jfoenix.controls.JFXTogglePane;
-import javafx.geometry.Insets;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.CornerRadii;
-import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 
 public class SeatController extends TicketController {
@@ -17,7 +12,9 @@ public class SeatController extends TicketController {
   private Integer posto;
 
   public void buttonClickedSeat(MouseEvent mouseEvent) {
-    seatItem.setStyle("-fx-background-color:#00ac00");
+    if (!seatItem.isDisabled()) {
+      seatItem.setStyle("-fx-background-color:#00ac00");
+    }
   }
 
   public void setNumberSeat(Integer numberSeat) {
@@ -26,6 +23,7 @@ public class SeatController extends TicketController {
 
   public void setOccupiedSeat(){
     seatItem.setStyle("-fx-background-color: #ff0000");
+    seatItem.setDisable(true);
     System.out.println("Posto occupato");
   }
 
